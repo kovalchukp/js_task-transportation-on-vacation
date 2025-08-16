78aab12d951d00daad6f532a909ccad8a5f90db6
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-08-16 17:37:39 -0600

    Solution propper coding

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,17 +6,20 @@
 function calculateRentalCost(days) {
   // write code here
   let total = 0;
+  const costOfRent = 40;
+  const basicDisc = 20;
+  const longTermDisc = 50;
 
   if (days < 3) {
-    total = days * 40;
+    total = days * costOfRent;
   }
 
   if (days >= 3 && days < 7) {
-    total = days * 40 - 20;
+    total = days * costOfRent - basicDisc;
   }
 
   if (days >= 7) {
-    total = days * 40 - 50;
+    total = days * costOfRent - longTermDisc;
   }
 
   return total;
